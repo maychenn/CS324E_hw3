352fb9906fbca076f0c513cd64093b67be290440
May <may99chen@gmail.com>
2020-02-12 20:16:36 -0600

    finished uniquewords + wordfreq

--- extract_words.py
+++ extract_words.py
@@ -2,41 +2,43 @@
 
 def main():
     #opens the txt file and stores the text
-    #file = open("anthem.txt", 'r')
+    file = open("anthem.txt", 'r')
+    
+    ###insert code for allwords here
 
-    #allwords = file.read().rstrip().split()
-    allwords = ["apple", "apple", "orange", "yes", "yes", "yes", "no"]
+    #allwords = ["apple", "apple", "orange", "yes", "yes", "yes", "no"]
     for word in allwords:
         word = word.lower()
 
     #word freq
-    word_freq = {}
+    word_freq = dict()
     for word in allwords:
         if (word in word_freq):
             word_freq[word] += 1
         else:
             word_freq[word] = 1
-
-    #sorts by increasing freq
-    word_freq = sorted(((y,x) for x,y in word_freq.items()))
-    
     print(word_freq)
+    
+    #sorts by increasing freq (word_freq is now a list of lists)
+    word_freq = sorted(([y,x] for x,y in word_freq.items()))
+    print(word_freq)
+    
     #writes to wordfrequency file
     word_frequency = open("wordfrequency.txt", 'w')
-    for word in word_freq:
-        line = str(word) + ": "
-        print(word_freq[word])
+    for freq in word_freq:
+        line = str(freq[0]) + ": " + freq[1] + "\n"
         word_frequency.write(line)
         
     #unique words
     unique_words = open("uniquewords.txt", 'w')
     
     for word in word_freq:
-        if (word_freq[word] == 1):
-            line = str(word) + "\n"
+        if (word[0] == 1):
+            line = str(word[1]) + "\n"
             unique_words.write(line)
-    print(unique_words)
+
     print("done")
+
     #file.close()
     
 main()
