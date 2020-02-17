bf06ca1e8ffbe15364cc81d7785527793520ac24
mw35497 <60294980+mw35497@users.noreply.github.com>
2020-02-16 18:39:34 -0600

    Add files via upload

--- extract_words.py
+++ extract_words.py
@@ -1,11 +1,32 @@
 #from collections import defaultdict
+import re #regular expression
 
 def main():
     #opens the txt file and stores the text
     file = open("anthem.txt", 'r')
     
-    ###insert code for allwords here
+    #allwords
+    #opens an alternate txt file and stores the text
+    file_alt = open("anthem.txt", "r", encoding = "utf-8")
+    
+    #stores text as a string with all lowercase characters 
+    allwords = file_alt.read().lower()
+    
+    #split string into list with non-alphabetical characters (or groups of characters) as delimiters
+    allwords = re.findall(r"[a-z]+", allwords)
+    
+    #append linebreak to all but last word
+    allwords_linebreaks = allwords.copy()
+    for word_index in range(len(allwords_linebreaks)-1):
+        allwords_linebreaks[word_index] = allwords_linebreaks[word_index]+"\n"
+        
+    #creates txt file for allwords output
+    allwords_outfile = open("allwords.txt", "w")
+    allwords_outfile.writelines(allwords_linebreaks)
+    file_alt.close()
+    allwords_outfile.close()
 
+    
     #allwords = ["apple", "apple", "orange", "yes", "yes", "yes", "no"]
     for word in allwords:
         word = word.lower()
